0d83374c3bd654978add72bff84ef50883792acb
“deligarbur” <deliagarciaburgos@gmail.com>
2024-05-22 14:03:08 +0200

    create landing & routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
+	<HashRouter>
 		<App />
-	</React.StrictMode>
+	</HashRouter>
 );
